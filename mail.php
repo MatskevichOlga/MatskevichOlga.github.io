bd7d199a3f684cf6e3dc698bdbcc4793e1074ef1
MATSKEVICH_OLGA <Pola221182@list.ru>
2020-12-17 20:56:24 +0300

    New_commit

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "Pola09@list.ru";
+$sitename = "Название сайта";
+
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["comment"]);
+$message = "Имя: $name \nПочта: $email \nТекст: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
